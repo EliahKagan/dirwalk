4cdc3e4e3b913acb96cf0c79efa85835faaec446
Eliah Kagan <degeneracypressure@gmail.com>
2024-12-11 13:01:46 -0500

    Don't bother passing any patterns

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ fn main() -> anyhow::Result<()> {
         .classify_untracked_bare_repositories(true)
         .emit_collapsed(Some(CollapsedEntriesEmissionMode::All))
         .symlinks_to_directories_are_ignored_like_directories(false)
-        .empty_patterns_match_prefix(true);
+        .empty_patterns_match_prefix(false);
     let index = repo.index()?;
-    for item in repo.dirwalk_iter(index, vec![":(top)"], Default::default(), options)? {
+    for item in repo.dirwalk_iter(index, Vec::<&str>::new(), Default::default(), options)? {
         println!("{:?}", item?.entry);
     }
     Ok(())
