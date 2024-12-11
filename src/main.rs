933da07e2ca0ad7bdf32ae0134897685e4a04ce9
Eliah Kagan <degeneracypressure@gmail.com>
2024-12-11 12:56:29 -0500

    Emit more kinds of entries
    The most of important of which is adding the missing
    `emit_tracked(true)` call.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use gix::dir::walk::{CollapsedEntriesEmissionMode, EmissionMode};
+
 fn main() -> anyhow::Result<()> {
     let repo = gix::discover(".")?;
     let options: gix::dirwalk::Options = repo
         .dirwalk_options()?
         .recurse_repositories(false)
-        .emit_pruned(false)
-        .emit_ignored(None)
+        .emit_pruned(true)
+        .emit_ignored(Some(EmissionMode::Matching))
         .for_deletion(None)
-        .emit_untracked(gix::dir::walk::EmissionMode::Matching)
-        .emit_empty_directories(false)
+        .emit_tracked(true)
+        .emit_untracked(EmissionMode::Matching)
+        .emit_empty_directories(true)
         .classify_untracked_bare_repositories(true)
-        .emit_collapsed(None)
+        .emit_collapsed(Some(CollapsedEntriesEmissionMode::All))
         .symlinks_to_directories_are_ignored_like_directories(false)
         .empty_patterns_match_prefix(true);
     let index = repo.index()?;
