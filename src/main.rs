00dacee6f38c4de1f2207efa71ff769635daa0b5
Eliah Kagan <degeneracypressure@gmail.com>
2024-12-11 08:30:06 -0500

    Set some options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 fn main() -> anyhow::Result<()> {
     let repo = gix::discover(".")?;
-    let options = repo.dirwalk_options()?;
-    println!("{options:?}");
+    let options: gix::dirwalk::Options = repo
+        .dirwalk_options()?
+        .recurse_repositories(false)
+        .emit_pruned(false)
+        .emit_ignored(None)
+        .for_deletion(None)
+        .emit_untracked(gix::dir::walk::EmissionMode::Matching)
+        .emit_empty_directories(false)
+        .classify_untracked_bare_repositories(true)
+        .emit_collapsed(None)
+        .symlinks_to_directories_are_ignored_like_directories(false)
+        .empty_patterns_match_prefix(true);
+    let index = repo.index()?;
+    for item in repo.dirwalk_iter(index, vec![":(top)"], Default::default(), options)? {
+        println!("{:?}", item?.entry);
+    }
     Ok(())
 }
